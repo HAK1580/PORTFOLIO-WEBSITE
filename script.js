d10c50ee660544f74a29f9f1f0146fd5e53fa97f
HAK1580 <aliofficial1580630@gmail.com>
2025-07-09 23:45:18 +0500

    updated portfolio

---
+++ script.js
@@ -0,0 +1,26 @@
+  const line1 = "M.HASSAN ALI KHAN";
+  const line2 = "a Front End Developer.";
+  let i = 0;
+  let j = 0;
+
+  function typeLine1() {
+    if (i < line1.length) {
+      const colored = `<span class="red-text">${line1.slice(0, i + 1)}</span>`;
+      document.getElementById("hero-line1").innerHTML = colored;
+      i++;
+      setTimeout(typeLine1, 120);
+    } else {
+      setTimeout(typeLine2, 500);
+    }
+  }
+
+  function typeLine2() {
+    if (j < line2.length) {
+      document.getElementById("hero-line2").innerHTML += line2.charAt(j);
+      j++;
+      setTimeout(typeLine2, 120);
+    }
+  }
+
+  window.onload = typeLine1;
+
