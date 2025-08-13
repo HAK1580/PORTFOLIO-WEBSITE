f8b68a0826d0a4b46eef82febc3f333f1a989690
HAK1580 <aliofficial1580630@gmail.com>
2025-08-12 17:45:11 -0700

    brand new portfolio

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function typeLine1() {
     const colored = `<span class="red-text">${line1.slice(0, i + 1)}</span>`;
     document.getElementById("hero-line1").innerHTML = colored;
     i++;
-    setTimeout(typeLine1, 120);
+    setTimeout(typeLine1,140);
   } else {
     setTimeout(typeLine2, 500);
   }
@@ -25,33 +25,13 @@ function typeLine2() {
 
 window.onload = typeLine1;
 
-let currentIndex = 0;
-const track = document.querySelector('.slider-track');
-const cards = document.querySelectorAll('.project-card');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
 
-function updateSlider() {
-  const cardWidth = cards[0].offsetWidth + 30; // card + gap
-  track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-}
-
-nextBtn.addEventListener('click', () => {
-  if (currentIndex < cards.length - 1) {
-    currentIndex++;
-    updateSlider();
+// JavaScript for scroll effect
+window.addEventListener("scroll", function () {
+  const nav = document.querySelector("nav");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
   }
 });
-
-prevBtn.addEventListener('click', () => {
-  if (currentIndex > 0) {
-    currentIndex--;
-    updateSlider();
-  }
-});
-
-  function scrollSlider(direction) {
-    const slider = document.querySelector('.project-slider');
-    const scrollAmount = 350;
-    slider.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
-  }
